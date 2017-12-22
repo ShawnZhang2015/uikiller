5b7e790add153f97a2a0948a0c4ef6de9ee37d18
shawnzhang <zxh1982@qq.com>
2017-12-22 10:10:19 +0800

    修改test8,增加隐藏节点的控制

--- assets/cases/uikiller/test8.js
+++ assets/cases/uikiller/test8.js
@@ -9,7 +9,10 @@ cc.Class({
 
     onLoad: function () {
         cc.log(this.name, ',onLoad');
-        this._item1.$Item.string = 'cocos1';
+        this._item1.$Item.string = '显示这个';
+        this.scheduleOnce(() => {
+            this._item1.active = true;
+        }, 2);
     },
 
 });
